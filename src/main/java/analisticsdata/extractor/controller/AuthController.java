e4286afbfd61fd9ac9fb4824c73b29a2c752ee60
unknown <deolhonofonte@gmail.com>
2023-07-04 23:12:34 -0300

    extractor load

--- src/main/java/analisticsdata/extractor/controller/AuthController.java
+++ src/main/java/analisticsdata/extractor/controller/AuthController.java
@@ -3,13 +3,22 @@ package analisticsdata.extractor.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import analisticsdata.extractor.dtos.ClientDto;
 
 @Controller
 public class AuthController {
 
-	@GetMapping(value = "/")
+	@GetMapping(value = "/Auth")
 	String Auth() {
 		return "auth/auth.html";
+	}
+	
+	@PostMapping(value="/validateclient")
+	void validateClient(ClientDto clientDto) {
+		System.out.println(clientDto.getCpf());
 	}
 }
---
+++ src/main/java/analisticsdata/extractor/dtos/ClientDto.java
@@ -0,0 +1,22 @@
+package analisticsdata.extractor.dtos;
+
+public class ClientDto {
+	
+	private String cpf;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public ClientDto(String cpf) {
+		this.cpf = cpf;
+	}
+	public ClientDto() {}
+	
+	
+	
+
+}
